a6e044e4a4f9e4a46a2d285162b32f133b15bc37
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-25 05:26:53 +0530

    Prevent sign in for inactive users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def active_for_authentication?
+    super and self.is_active?
+  end
+
+  def inactive_message
+    self.is_active? ? super : "User is not allowed to sign in! Please contact your admin"
+  end
 end
