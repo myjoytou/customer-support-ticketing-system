dcc1538c3ffcc8a0a7a039137d0f9d0513663681
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:31:30 +0530

    Add routes for customer controller action and support controller action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get 'customers/get_previous_tickets', to: 'customer#get_previous_tickets'
+  get 'supports/get_pending_tickets', to: 'support#get_pending_tickets'
+  get 'supports/get_closed_tickets', to: 'support#get_closed_tickets'
+  post 'supports/process_pending_tickets', to: 'support#process_pending_tickets'
 end
