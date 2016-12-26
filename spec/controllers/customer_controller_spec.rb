67f8cb71955a2210567895c8523ed80588c38446
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:40:34 +0530

    Test cases for customer controller

---
+++ spec/controllers/customer_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe CustomerController, type: :controller do
+  let(:user) {FactoryGirl.create(:user)}
+
+  it "does not allow to access data without signin" do
+    get "get_previous_tickets", format: :json
+    expect(response.status).to eq(401)
+    puts "================ #{response.body}"
+  end
+
+  it "does return status as Success when trying to create ticket" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in FactoryGirl.create(:user)
+    get "get_previous_tickets", format: :json
+    expect(response.status).to eq(200)
+    puts "=========success======= #{response.body}"
+  end
+
+end
