96126f7ccea58cdabebed8ee6bd2646b6d9a6003
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:44:16 +0530

    Factories for testing

---
+++ spec/factories/tickets.rb
@@ -0,0 +1,41 @@
+require 'factory_girl'
+
+
+FactoryGirl.define do
+  factory :ticket  do
+    ignore do
+      user nil
+    end
+    name {Faker::Name.name }
+    short_description { Faker::Lorem.words(rand(10)+1).join(" ") }
+    long_description { Faker::Lorem.words(rand(30)+1).join(" ") }
+    status {"Open"}
+    user_id {1}
+
+    trait :with_closed_status do
+      after :create do |ticket|
+        ticket.status = 'Closed'
+        ticket.save!
+      end
+    end
+
+    trait :with_user do
+      after :create do |ticket, evaluator|
+        ticket.user_id = evaluator.user.id
+        ticket.save!
+      end
+    end
+
+    factory :closed_ticket, traits: [:with_closed_status]
+    factory :closed_ticket_with_user, traits: [:with_closed_status, :with_user]
+    # trait :with_support_role do
+    #   after :create do |user|
+    #     user.support = true
+    #     user.save!
+    #   end
+    # end
+    #
+    # factory :support, traits: [:with_support_role]
+
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,27 @@ require 'factory_girl'
 
 
 FactoryGirl.define do
-  factory :user, :class => WorkFlow::User  do
-    # name {Faker::Name.name }
+  factory :user  do
+    name {Faker::Name.name }
     email {name.downcase.gsub(/[.\ ]*/, "") + "@gmail.com" }
     password {"123456"}
     password_confirmation {"123456"}
+
+    trait :with_support_role do
+      after :create do |user|
+        user.support = true
+        user.save!
+      end
+    end
+
+    trait :with_inactive_status do
+      after :create do |user|
+        user.is_active = false
+        user.save!
+      end
+    end
+
+    factory :support, traits: [:with_support_role]
+    factory :inactive_user, traits: [:with_inactive_status]
   end
 end
