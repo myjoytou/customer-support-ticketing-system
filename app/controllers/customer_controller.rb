abb9c09a447bac64218dff14718ea70dcb0e9593
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:30:37 +0530

    Add actions to customer controller

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,2 +1,19 @@
 class CustomerController < ApplicationController
+  respond_to :json
+
+  def get_previous_tickets
+    tickets = current_user.tickets
+    respond_with({data: tickets, status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def create_new_ticket
+    validate_params params
+    Ticket.create_support_ticket(params, current_user)
+    respond_with({data: '', status: @@status[:success], error_message: ''})
+  end
+
+  private
+  def validate_params(params)
+    raise "Name should be present!" if params[:name].blank?
+  end
 end
