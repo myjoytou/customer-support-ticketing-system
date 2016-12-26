22b775180768c319485797bc6b7017a95acde7ba
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:36:43 +0530

    Adding create method and process method on ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,33 @@
 class Ticket < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :status, presence: true
+
+  @@possible_status = {
+      open: "Open",
+      processing: "Processing",
+      closed: "Closed"
+  }.freeze
+  cattr_reader :possible_status
+
+  scope :open_tickets, -> { where(status: Ticket.possible_status[:open]) }
+  scope :processing, -> { where(status: Ticket.possible_status[:processing]) }
+  scope :closed_tickets, -> { where(status: Ticket.possible_status[:closed]) }
+
+  def self.create_support_ticket(params, current_user)
+    ticket = Ticket.new
+    ticket.name = params[:name]
+    ticket.short_description = params[:short_description]
+    ticket.long_description = params[:long_description]
+    ticket.status = Ticket.possible_status[:open]
+    ticket.user_id = current_user.id
+    ticket.save!
+  end
+
+  def process_ticket(status)
+    self.status = status
+    self.save!
+  end
+
 end
