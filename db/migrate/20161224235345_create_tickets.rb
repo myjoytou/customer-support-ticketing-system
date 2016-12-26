11be37124aa1a751e9579aa91d68e128e87eda3c
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:39:06 +0530

    Migration for ticket

--- db/migrate/20161224235345_create_tickets.rb
+++ db/migrate/20161224235345_create_tickets.rb
@@ -1,7 +1,12 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
-
+      t.belongs_to :user, index: true
+      t.string :name, null: false
+      t.text :short_description
+      t.text :long_description
+      t.string :image_url
+      t.string :status, null: false
       t.timestamps
     end
   end
