d91ff2b2a7d6c4709915eb05c62257dc56fc3283
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-26 21:31:00 +0530

    Add actions to support controller

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,2 +1,44 @@
 class SupportController < ApplicationController
+  respond_to :json, :pdf
+  before_action :check_support_user
+
+
+
+  def get_pending_tickets
+    tickets = Ticket.open_tickets.where(user_id: current_user.id)
+    respond_with({data: tickets, status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def process_pending_tickets
+    validate_params
+    ticket = Ticket.open_tickets.where(params[:ticket_id]).first if Ticket.open_tickets.present?
+    raise "Ticket not present" if ticket.blank?
+    ticket.process_ticket params[:status]
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def get_closed_tickets
+    @closed_tickets = Ticket.closed_tickets.where("created_at >= '#{Time.now - 1.month}' and user_id = #{current_user.id}")
+    raise "No closed ticket in past month!" if @closed_tickets.blank?
+    respond_to do |format|
+      format.html
+      format.json
+      format.pdf do
+        pdf = ClosedTicketPdf.new(@closed_tickets)
+        filename = File.join(Rails.root, "app/pdfs", "ClosedTicketReport.pdf")
+        pdf.render_file filename
+        # send_data pdf.render, filename: "ClosedTicketReport", type: "application/pdf", disposition: "inline"
+        send_file filename, filename: "ClosedTicketReport.pdf", type: "application/pdf"
+      end
+    end
+  end
+
+  private
+  def validate_params
+    raise "Ticket id not present" if params[:ticket_id].blank?
+    raise "Status not present" if params[:status].blank?
+  end
+  def check_support_user
+    raise "Not Authorised!" if !current_user.support?
+  end
 end
