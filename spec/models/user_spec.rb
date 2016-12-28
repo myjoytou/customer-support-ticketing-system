0f8304ec6aa606adf07ff89011507eb1b611b358
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-29 03:08:13 +0530

    New test cases for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:name].size).to eq(1)
   end
+
+  it "does assign support role to user" do
+    user.assign_role(:support)
+    expect(user.support).to eq(true)
+  end
+
+  it "does assign admin role to user" do
+    user.assign_role(:admin)
+    expect(user.admin).to eq(true)
+  end
+
+  it "does deny support role to user" do
+    user = FactoryGirl.create(:support)
+    user.deny_role(:support)
+    expect(user.support).to eq(false)
+  end
+
+  it "does deny admin role to user" do
+    user = FactoryGirl.create(:admin)
+    user.deny_role(:admin)
+    expect(user.admin).to eq(false)
+  end
+
 end
