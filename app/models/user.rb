ed02a7a121a6be4f05950c06f803f5bf0cc89605
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-29 00:14:35 +0530

    Adding support for user activation and deactivation through admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true
+
+  has_many :tickets
+
+  def deactivate
+    self.is_active = false
+    self.save!
+  end
+
+  def activate
+    self.is_active = true
+    self.save!
+  end
+
   def active_for_authentication?
     super && self.is_active?
   end
@@ -11,7 +25,5 @@ class User < ApplicationRecord
   def inactive_message
     self.is_active? ? super : "User is not allowed to sign in! Please contact your admin"
   end
-  validates :name, presence: true
 
-  has_many :tickets
 end
