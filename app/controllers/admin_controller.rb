7511ea8019aff9229a7d5ec2f80e3ef10ad00c68
Vivek Kumar <vivek.kumar@infibeam.net>
2016-12-29 03:05:20 +0530

    New actions for Admin User

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,15 +6,8 @@ class AdminController < ApplicationController
   end
 
   def get_all_users
-    respond_with({data: User.all, status: @@status[:success], error_message: ''}, location: '/')
-  end
-
-  def deactivate_user
-    validate_params
-    user = User.where(id: params[:user_id]).first
-    raise "User not present!" if user.blank?
-    user.deactivate
-    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+    user = User.all
+    respond_with({data: user, status: @@status[:success], error_message: ''}, location: '/')
   end
 
   def activate_user
@@ -25,12 +18,52 @@ class AdminController < ApplicationController
     respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
   end
 
-  def process_pending_tickets
+  def deactivate_user
     validate_params
-    ticket = Ticket.open_tickets.where(params[:ticket_id].to_s).first if Ticket.open_tickets.present?
+    user = User.where(id: params[:user_id]).first
+    raise "User not present!" if user.blank?
+    user.deactivate
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def assign_support_role
+    validate_params
+    user = User.where(id: params[:user_id]).first
+    raise "User not present!" if user.blank?
+    user.assign_role(:support)
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def deny_support_role
+    validate_params
+    user = User.where(id: params[:user_id]).first
+    raise "User not present!" if user.blank?
+    user.deny_role(:support)
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def assign_admin_role
+    validate_params
+    user = User.where(id: params[:user_id]).first
+    raise "User not present!" if user.blank?
+    user.assign_role(:admin)
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def deny_admin_role
+    validate_params
+    user = User.where(id: params[:user_id]).first
+    raise "User not present!" if user.blank?
+    user.deny_role(:admin)
+    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+  end
+
+  def process_pending_tickets
+    validate_params_for_ticket
+    ticket = Ticket.open_tickets.where(id: params[:ticket_id]).first if Ticket.open_tickets.present?
     raise "Ticket not present" if ticket.blank?
     ticket.process_ticket params[:status], current_user
-    respond_with({data: '', status: @@status[:success], error_message: ''}, location: '/')
+    respond_with({data: ticket, status: @@status[:success], error_message: ''}, location: '/')
   end
 
   def get_pending_tickets
@@ -59,6 +92,12 @@ class AdminController < ApplicationController
   end
 
   private
+
+  def validate_params_for_ticket
+    raise "Ticket id not present!" if params[:ticket_id].blank?
+    raise "Status not present!" if params[:status].blank?
+  end
+
   def validate_params
     raise "User id not present!" if params[:user_id].blank?
   end
